6bfbc51129acb6cb94ec74ffe395d5e405fe81d8
nangcr <nangcr@gmail.com>
2023-03-08 13:18:35 +0800

    support SSE(Server-Sent Events)

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bytes"
-	"context"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"encoding/json"
+	"github.com/akrylysov/algnhsa"
+	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
 )
@@ -18,7 +18,6 @@ func sendRequest(apiKey string, payload []byte) ([]byte, int, error) {
 	// 创建一个新的 HTTP 请求
 	req, err := http.NewRequest(http.MethodPost, openaiURL, bytes.NewBuffer(payload))
 	if err != nil {
-		// 如果出现错误，返回一个带有错误信息和 500 状态码的响应
 		return nil, http.StatusInternalServerError, err
 	}
 
@@ -37,7 +36,6 @@ func sendRequest(apiKey string, payload []byte) ([]byte, int, error) {
 		// 如果出现错误，返回一个带有错误信息和 500 状态码的响应
 		return nil, http.StatusInternalServerError, err
 	}
-	// 释放响应体的资源
 	defer resp.Body.Close()
 
 	// 读取响应体的内容
@@ -47,33 +45,97 @@ func sendRequest(apiKey string, payload []byte) ([]byte, int, error) {
 		return nil, http.StatusInternalServerError, err
 	}
 
-	// 返回包含响应体内容、响应状态码和 nil 错误
+	// 返回响应体的内容，状态码和错误信息
 	return body, resp.StatusCode, nil
 }
 
-// handler 是 Lambda 函数的处理函数
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// 从请求头部中获取 Authorization 头部的值
-	apiKey := request.Headers["authorization"]
-	// 将请求体转换为字节数组
-	payload := []byte(request.Body)
-
-	// 调用 sendRequest 函数发送 HTTP 请求并获取响应
-	response, statusCode, err := sendRequest(apiKey, payload)
+func sendRequestSSE(ctx *gin.Context, apiKey string, payload []byte) {
+	// 创建一个新的 HTTP 请求
+	req, err := http.NewRequest(http.MethodPost, openaiURL, bytes.NewBuffer(payload))
 	if err != nil {
-		// 如果出现错误，返回一个带有错误信息和 500 状态码的响应
-		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: http.StatusInternalServerError}, nil
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	// 构造响应头部
-	headers := make(map[string]string)
-	headers["Content-Type"] = "application/json"
+	// 设置请求头部
+	req.Header.Set("Content-Type", "application/json")
+	if apiKey != "" {
+		req.Header.Set("Authorization", apiKey)
+	}
+	req.Header.Set("Accept", "text/event-stream")
 
-	// 返回带有 HTTP 响应体和响应头部的 APIGatewayProxyResponse 对象
-	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: statusCode, Headers: headers}, nil
+	// 执行HTTP请求
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	// 设置响应头部
+	ctx.Header("Content-Type", "text/event-stream")
+	ctx.Header("Cache-Control", "no-cache")
+	ctx.Header("Connection", "keep-alive")
+
+	// 转发响应体
+	_, err = io.Copy(ctx.Writer, resp.Body)
+	if err != nil {
+		ctx.String(http.StatusInternalServerError, err.Error())
+		return
+	}
 }
 
 func main() {
+	r := gin.Default()
+
+	// 跨域设置
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	})
+
+	// 任意路径都会被匹配
+	r.Any("/*any", func(ctx *gin.Context) {
+		// 读取请求头部的 Authorization 字段
+		apiKey := ctx.GetHeader("authorization")
+
+		// 读取请求体的内容
+		payload, err := io.ReadAll(ctx.Request.Body)
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+		body := map[string]interface{}{}
+		err = json.Unmarshal(payload, &body)
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		// 判断是否是 SSE 请求
+		if sse, ok := body["stream"]; ok && sse == true {
+			sendRequestSSE(ctx, apiKey, payload)
+			return
+		} else {
+			response, statusCode, err := sendRequest(apiKey, payload)
+			if err != nil {
+				ctx.String(http.StatusInternalServerError, err.Error())
+				return
+			}
+			ctx.Data(statusCode, "application/json", response)
+		}
+
+	})
+
 	// 启动 Lambda 函数
-	lambda.Start(handler)
+	algnhsa.ListenAndServe(r, nil)
 }
